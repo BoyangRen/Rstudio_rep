89f2eae58bf222e025a6f9bd2f68f6f516120b5c
BoyangRen <renboyang@outlook.com>
2024-04-13 15:53:04 -0400

    20240413.1.2

--- test_kfold.R
+++ test_kfold.R
@@ -1,1 +1,23 @@
+#Ref:https://www.youtube.com/watch?v=vTa74Ja8R8A&ab_channel=DataSciencewithYan
+
+
 install.packages("ISLR2")
+library(ISLR2)
+data("Auto")
+
+
+#1.training and testing set method.
+n=length(Auto$mpg)
+n
+set.seed(1)
+Z=sample(n,n/2)
+training=Auto[Z,]
+testing=Auto[-Z,]
+model1=lm(mpg~weight+horsepower+acceleration,
+          data=training)
+summary(model1)
+pred.test=predict(model1,testing)
+plot(testing$mpg,pred.test)
+mean((pred.test-testing$mpg)^2)
+
+#2.leave one out cross validation
